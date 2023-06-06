b8001a2342788571a4b5d45487a91d04b03fcdb9
momo-p <contact@relay.akimitsu.dev>
2023-06-06 18:11:27 +0700

    refactor: only run test on file with `.test.ts` suffix

---
+++ test/example.test.ts
@@ -0,0 +1,7 @@
+import * as assert from "assert";
+
+describe("example test.", () => {
+  it("is 1 equals 1?", () => {
+    assert.equal(1, 1);
+  });
+});
--- test/metadata.ts
+++
@@ -1,7 +0,0 @@
-import * as assert from "assert";
-
-describe("get video metadata.", () => {
-  it("mock up test for boilerplate reason.", () => {
-    assert.equal(1, 1);
-  });
-});
